ee9b42012d3a043627f70ec2c76ddd2fa16dcc93
1CoolDavid <davidrudo@hotmail.com>
2018-06-29 09:03:14 -0400

    [ADD] Different Speed Chickens

---
+++ MediumChick.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+c1()
+{
+	echo "${!1}          _-----_"
+        echo "${!1}         -     0 -"
+        echo "${!1}         -        >"
+        echo "${!1}      ----       - "
+        echo "${!1}     -- ~~~      - "
+        echo "${!1}     ------------  "
+        echo "${!1}        /     /    "
+        echo "${!1}       ^     ^     "
+	sleep 0.05
+}
+c2()
+{
+	echo "${!1}          _-----_"
+        echo "${!1}         -     0 -"
+        echo "${!1}         -        >"
+        echo "${!1}      ----       - "
+        echo "${!1}     -- ~~~      - "
+        echo "${!1}     ------------  "
+        echo "${!1}        |     |    "
+        echo "${!1}        ^     ^    "
+	sleep 0.05
+}
+c3()
+{
+	echo "${!1}          _-----_"
+        echo "${!1}         -     0 -"
+        echo "${!1}         -        >"
+        echo "${!1}      ----       - "
+        echo "${!1}     -- ~~~      - "
+        echo "${!1}     ------------  "
+        echo "${!1}        \     \    "
+        echo "${!1}         ^     ^   "
+	sleep 0.05
+}
+clear
+lines=1
+COUNTER=1
+CATCHER=1
+STR=""
+echo -e "\n\n\n\n\n\n\n\n\n\033[8F\033[s"
+while [ 1 = 1 ]
+do 	lines=$(($(tput cols)-19))
+        if [ "$COUNTER" -gt 1 ]; then
+	until [ "$CATCHER" -eq "$COUNTER" ]; do
+          STR+=" "
+           ((CATCHER++))
+        done
+    CATCHER=0
+    fi
+    if [ "$COUNTER" -gt "$lines" ]; then
+	COUNTER=0
+	STR=""
+	clear
+    fi
+    echo -e "\033[u     "; c1 STR
+    echo -e "\033[u     "; c2 STR
+    echo -e "\033[u     "; c3 STR
+    ((COUNTER+=2))
+    STR=""
+done
